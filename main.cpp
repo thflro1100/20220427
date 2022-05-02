2443a878aa960823f54d4c7eb931bde85e80db83
hyunjaesong <thflro1100@naver.com>
2022-05-02 09:29:01 +0900

    사칙연산

--- main.cpp
+++ main.cpp
@@ -3,8 +3,22 @@ using namespace std;
 
 int main()
 {
-cout << "HelloWorld" <<endl;
+	int a = 5, b = 3;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+
+
+	float c = 8, d = 3;
+
+	cout << c + d << endl;
+	cout << c - d << endl;
+	cout << c * d << endl;
+	cout << c / d << endl;
+
 
 	return 0;
 }
-//¼öÁ¤¿ë Ã·»è
